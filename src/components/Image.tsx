29cb638bdd057b395db2f7efb8cad6a608d26263
Dayton Ellwanger <daytonellwanger@gmail.com>
2021-02-15 16:34:59 -0800

    Video thumbnails

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MediaAttachmentData } from '../Posts';
+import './Image.css';
 
 interface P {
     data: MediaAttachmentData;
@@ -8,13 +9,15 @@ interface P {
 interface S {
     content: string;
     webViewLink: string;
+    thumbnailLink: string;
 }
 
 export default class Image extends React.Component<P, S> {
 
     state: S = {
         content: '',
-        webViewLink: ''
+        webViewLink: '',
+        thumbnailLink: ''
     }
 
     async componentDidMount() {
@@ -25,18 +28,28 @@ export default class Image extends React.Component<P, S> {
     }
 
     render() {
-        return (
-            <a href={this.state.webViewLink}>
-                <img src={`data:image/jpeg;base64,${btoa(this.state.content)}`} className="_2yuc _3-96" />
-            </a>
-        );
+        if (this.state.webViewLink && !this.state.content) {
+            return (
+                <a href={this.state.webViewLink} target="_blank" rel="noopener noreferrer">
+                    <div className='video-thumbnail'>
+                        <img src={this.state.thumbnailLink} className="_2yuc _3-96" />
+                    </div>
+                </a>
+            );
+        } else {
+            return (
+                <a href={this.state.webViewLink} target="_blank" rel="noopener noreferrer">
+                    <img src={`data:image/jpeg;base64,${btoa(this.state.content)}`} className="_2yuc _3-96" />
+                </a>
+            );
+        }
     }
 
 }
 
-let getPhotoDataQueue: Promise<{ content: string, webViewLink: string } | undefined> = Promise.resolve(undefined);
+let getPhotoDataQueue: Promise<S | undefined> = Promise.resolve(undefined);
 
-async function getPhotoData(uri: string): Promise<{ content: string, webViewLink: string } | undefined> {
+async function getPhotoData(uri: string): Promise<S | undefined> {
     getPhotoDataQueue = getPhotoDataQueue.then(async () => {
         const uriPieces = uri.split('/');
         const fileName = uriPieces[uriPieces.length - 1];
@@ -49,11 +62,15 @@ async function getPhotoData(uri: string): Promise<{ content: string, webViewLink
             return;
         }
         // TODO: run these in parallel?
-        const photoContent = (await gapi.client.drive.files.get({ fileId: photoFileId, alt: 'media' })).body;
-        const photoInfo = (await gapi.client.drive.files.get({ fileId: photoFileId, fields: 'webViewLink' })).result;
+        let photoContent = '';
+        if (fileName.endsWith('jpg')) {
+            photoContent = (await gapi.client.drive.files.get({ fileId: photoFileId, alt: 'media' })).body;
+        }
+        const photoInfo = (await gapi.client.drive.files.get({ fileId: photoFileId, fields: 'webViewLink, thumbnailLink' })).result;
         return {
             content: photoContent,
-            webViewLink: photoInfo.webViewLink!
+            webViewLink: photoInfo.webViewLink!,
+            thumbnailLink: photoInfo.thumbnailLink!
         };
     });
     return getPhotoDataQueue;
