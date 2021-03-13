9aab0d4db1607610e0c9bda357397abd94ed88a8
Dayton Ellwanger <daytonellwanger@gmail.com>
2021-03-12 21:13:10 -0800

    Retries with exponential backoff

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
 export function getTimeString(timestamp: number) {
-    return moment(timestamp*1000).format('MMM Do, YYYY, h:mm A');
+    return moment(timestamp * 1000).format('MMM Do, YYYY, h:mm A');
 }
 
 export function decodeString(raw: string) {
@@ -67,6 +67,29 @@ export async function getFileData(uri: string): Promise<FileData | undefined> {
     };
 }
 
+async function executeWithExponentialBackoff(executeRequest: () => Promise<any>, delay = 1000, attemptNumber = 1): Promise<any> {
+    if (attemptNumber === 6) {
+        throw new Error('Could not upload'); 
+    }
+    let didThrow = false;
+    let result: any;
+    try {
+        result = await executeRequest();
+    } catch {
+        didThrow = true;
+    }
+    if (didThrow || (typeof result.status === 'number' && result.status !== 200)) {
+        await new Promise<void>((resolve, reject) => {
+            setTimeout(() => resolve(), delay);
+        });
+        attemptNumber++;
+        delay = 2*delay;
+        return executeWithExponentialBackoff(executeRequest, delay, attemptNumber);
+    } else {
+        return result;
+    }
+}
+
 type PendingRequest = {
     executeRequest: () => Promise<any>,
     resolveResult: (result: any) => void
@@ -74,14 +97,14 @@ type PendingRequest = {
 
 class RequestQueue {
 
-    private static readonly maxParallelRequests = 30;
+    private static readonly maxParallelRequests = 25;
     private activeRequests = 0;
     private pendingRequests: PendingRequest[] = [];
 
     public async request<T>(executeRequest: () => Promise<T>): Promise<T> {
         if (this.activeRequests < RequestQueue.maxParallelRequests) {
             this.activeRequests++;
-            const result = await executeRequest();
+            const result = await executeWithExponentialBackoff(executeRequest);
             this.activeRequests--;
             this.processNextRequest();
             return result;
