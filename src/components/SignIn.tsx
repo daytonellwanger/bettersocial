0577dde56be522918583b423df5edd84567c4f70
Dayton Ellwanger <daytonellwanger@gmail.com>
2021-04-26 21:16:51 -0700

    Improved sign-in page

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import { Container, Link, Tooltip, Typography } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
+import InfoIcon from '@material-ui/icons/Info';
 
 interface P {
     signIn: () => void;
@@ -10,8 +11,17 @@ export default class SignIn extends React.Component<P> {
 
     public render() {
         return (
-            <Container style={{ paddingTop: '1em' }}>
-                <Button variant="contained" color="primary" onClick={this.props.signIn}>Sign In</Button>
+            <Container style={{ height: '100%', display: 'flex', flexDirection: 'row', alignItems: 'center' }} maxWidth="md">
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                    <Typography variant="h2">Welcome to Social Freedom</Typography>
+                    <Button variant="contained" color="primary" onClick={this.props.signIn} style={{ marginTop: '1.5em', marginBottom: '1.5em' }}>Sign In</Button>
+                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                        <Link href="https://google.com" target="_blank" color="secondary" underline="always">Why Google sign in?</Link>
+                        <Tooltip title="Google is a popular platform with free cloud storage that has a respectable privacy policy. Click the link to learn more.">
+                            <InfoIcon color="secondary" fontSize="small" style={{ marginLeft: '.2em' }}/>
+                        </Tooltip>
+                    </div>
+                </div>
             </Container>
         );
     }
